da38b4e307ebd53d74fae2f29c3adc578e39a6ff
Chen <33944427855@qq.com>
2022-07-18 11:04:45 +0800

    hello

--- Data Structures And Algorithms/src/com/queue/java/ArrayQueueDemo.java
+++ Data Structures And Algorithms/src/com/queue/java/ArrayQueueDemo.java
@@ -1,4 +1,5 @@
 package com.queue.java;
+import java.util.Scanner;
 
 /**
  * @Name NumberTwoPlayer
@@ -6,9 +7,53 @@ package com.queue.java;
  */
 public class ArrayQueueDemo {
     public static void main(String[] args) {
-
-
-
+        //测试
+        //创建一个队列
+       ArrayQueue queue = new ArrayQueue(3);
+       char key = ' ';//接收用户输入
+       Scanner scanner = new Scanner(System.in);
+       boolean loop = true;
+       //输出一个菜单
+        while(loop){
+            System.out.println("s(show): 显示队列");
+            System.out.println("e(exit): 退出队列");
+            System.out.println("a(add): 添加数据到队列");
+            System.out.println("g(get): 从队列去除数据");
+            System.out.println("h(head): 查看队列头的数据");
+            key = scanner.next().charAt(0);//接收一个字符
+            switch(key){
+                case's':
+                    queue.showQueue();
+                case'a':
+                    System.out.println("输出一个数");
+                    int value = scanner.nextInt();
+                    queue.addQueue(value);
+                    break;
+                case'g':
+                    try{
+                        int res = queue.getQueue();
+                        System.out.printf("取出的数据是%d\n", res);
+                    }catch(Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case'h':
+                    try{
+                        int res = queue.headQueue();
+                        System.out.printf("队列头的数据是%d\n", res);
+                    }catch(Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case'e': //退出
+                    scanner.close();
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println("程序退出");
 
     }
 }
