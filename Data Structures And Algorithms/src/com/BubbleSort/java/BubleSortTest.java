ac7c5565d18adf5bd523054f38d5dfebec417379
Chen <33944427855@qq.com>
2022-07-18 22:00:08 +0800

    第四次提交

---
+++ Data Structures And Algorithms/src/com/BubbleSort/java/BubleSortTest.java
@@ -0,0 +1,23 @@
+package com.BubbleSort.java;
+
+/**
+ * @Name NumberTwoPlayer
+ * @create 2022-07-18-21:49
+ */
+public class BubleSortTest {
+    public static void main(String[] args) {
+        int[] arr = {1, 23, 17, 19, 0, 33, 97};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int test = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = test;
+                }
+            }
+        }
+        for(int i : arr){
+            System.out.println(i);
+        }
+    }
+}
