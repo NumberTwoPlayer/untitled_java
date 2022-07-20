76696627bb9f62782df39525f4f7b619ecb01d98
Chen <33944427855@qq.com>
2022-07-20 20:43:40 +0800

    第N次提交

--- Multithreading/src/com/Multithreading/exer/ThreadDemo.java
+++ Multithreading/src/com/Multithreading/exer/ThreadDemo.java
@@ -11,13 +11,38 @@ package com.Multithreading.exer;
 
 public class ThreadDemo {
     public static void main(String[] args) {
-        MyThread1 m1 = new MyThread1();
-        MyThread2 m2 = new MyThread2();
+        //MyThread1 m1 = new MyThread1();
+        //MyThread2 m2 = new MyThread2();
 
-        m1.start();
-        m2.start();
+        //m1.start();
+        //m2.start();
+
+        //创建Thread类的匿名子类的方式
+        new Thread() {
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    if (i % 2 == 0) {
+                        System.out.println(Thread.currentThread().getName() + ":" + i);
+                    }
+                }
+            }
+        } .start();
+
+        new Thread(){
+            public void run(){
+                for(int i  = 0; i < 100; i++){
+                    if(i % 2 != 0){
+                        System.out.println(Thread.currentThread().getName() + ":" + i);
+                    }
+                }
+            }
+        }.start();
+
+
     }
 }
+
+
 
 
 class MyThread1 extends Thread{
