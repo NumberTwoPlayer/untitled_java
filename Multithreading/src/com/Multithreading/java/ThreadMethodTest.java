8737c1251e47e8ad15ccfc4e85bd26f3926657c6
Chen <33944427855@qq.com>
2022-07-23 20:23:45 +0800

    第N次提交

--- Multithreading/src/com/Multithreading/java/ThreadMethodTest.java
+++ Multithreading/src/com/Multithreading/java/ThreadMethodTest.java
@@ -13,6 +13,19 @@ package com.Multithreading.java;
  * 9、sleep(long millitime)：让当前线程“睡眠”指定的millitime毫秒，在指定的millittime毫秒时间内。当前线程处于堵塞状态
  * 10、isAlive()：判断当前线程是否存活
  *
+ * 线程的优先级：
+ * 1、
+ * MAX_PRIORITY: 10
+ * MIN_PRIORITY: 1
+ * NORM_PRIORITY: 5 --->默认优先级
+ *
+ * 2、如何获取和设置当前线程的优先级
+ * getPriority():获取线程的的优先级
+ * setPriority(int p):设置线程的优先级
+ *
+ * 说明：高优先级的线程要抢占低优先级线程CPU的执行权
+ * 但只是从概率
+ *
  *
  * @Name NumberTwoPlayer
  * @create 2022-07-22-19:08
@@ -24,11 +37,11 @@ class HelloThread extends Thread{
         for (int i = 0; i < 100; i++) {
             if(i % 2 == 0){
 
-                try {
-                    sleep(1000);
-                }catch (InterruptedException e){
-                    e.printStackTrace();
-                }
+//                 try{
+//                    sleep(1000);
+//                }catch (InterruptedException e){
+//                    e.printStackTrace();
+//                }
 
                 System.out.println(Thread.currentThread().getName() + ":" + i);
             }
@@ -49,25 +62,28 @@ public class ThreadMethodTest {
 
         HelloThread s1 = new HelloThread("Thread: 1: ");
 
-        s1.setName("线程一");
+//        s1.setName("线程一");
+        //设置分线程的优先级
+        s1.setPriority(Thread.MAX_PRIORITY);
 
         s1.start();
 
         //给主线程命名
         Thread.currentThread().setName("主线程");
+        Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
 
         for (int i = 0; i < 100; i++) {
             if(i % 2 == 0){
-                System.out.println(Thread.currentThread().getName() + ":" + i);
+                System.out.println(Thread.currentThread().getName() + ":"+ Thread.currentThread().getPriority()+ ":" + i);
             }
 
-            if(i == 20){
-                try {
-                    s1.join();
-                } catch (InterruptedException e){
-                    e.printStackTrace();
-                }
-            }
+//            if(i == 20){
+//                try {
+//                    s1.join();
+//                } catch (InterruptedException e){
+//                    e.printStackTrace();
+//                }
+//            }
         }
 
         System.out.println(s1.isAlive());
